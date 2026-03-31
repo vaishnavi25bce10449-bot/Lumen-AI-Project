312bbee49796c6b6949f103836bc5e8ccafb6324
Vaishnavi <vaishnavi.25bce10449@vitbhopal.ac.in>
2026-03-31 12:09:13 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,5 +1,5 @@
 """
-# LUMEN AI - Model Logic
+LUMEN AI - Model Logic PROJECT
 Using Isolation Forest for Unsupervised Outlier Detection.
 """
 
@@ -9,15 +9,11 @@ import pandas as pd
 def train_and_predict(features_df, sensitivity=0.1):
     """
     Isolation Forest algorithm apply karta hai.
-    -1 = Anomaly (Outlier)
-    1  = Normal Data
     """
     # Model define karna
-    # 'contamination' parameter batata hai ki kitne % anomalies expected hain
     clf = IsolationForest(contamination=sensitivity, random_state=42, n_estimators=100)
     
     # Fit and Predict on numerical columns only
-    # Hum 'RawText' column ko exclude karenge prediction ke waqt
     numeric_cols = ['Severity', 'Length', 'Complexity']
     features_df['anomaly_score'] = clf.fit_predict(features_df[numeric_cols])
     
